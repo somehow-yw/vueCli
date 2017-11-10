f3dcc25fe0676216e34af2255ab9eaa7811b5879
yangyumei <yumei.yang@zdongpin.com>
2017-11-10 17:44:52 +0800

    :construction_worker: table-component

--- src/components/bar.vue
+++ src/components/bar.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
-      <h1>按需加载</h1>
+    <tab-bar :tabs="tabs" />
   </div>
 </template>
 <script>
+import tabBar from './tabBar.vue'
 export default {
+  components: {tabBar},
   name: 'bar',
   data () {
     return {
-
+      tabs: [
+        {key: 'user', name: '用户管理'},
+        {key: 'role', name: '角色管理'}
+      ]
     }
   },
   methods: {
---
+++ src/components/tabBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane v-for="(tab, i) in tabs" :key="tab.key" :label="tab.name" :name="tab.key">{{tab.name}}</el-tab-pane>
+      </el-tabs>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      activeName: this.tabs[1].key
+    }
+  },
+  props: ['tabs'],
+  methods: {
+    handleClick (tab) {
+      console.log('tab', tab.name)
+    }
+  }
+}
+</script>
